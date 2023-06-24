f76b07c78ab576f540f8f15302690376816d9413
Kyoko <gatomghosh@gmail.com>
2023-06-24 17:10:28 +0530

    signup.components.jsx modified

--- src/components/signup.components.jsx
+++ src/components/signup.components.jsx
@@ -10,25 +10,46 @@ const eye = <FontAwesomeIcon icon={faEye} />;
 
 
 const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 const [passwordShown, setPasswordShown] = useState(false);
   const togglePasswordVisiblity = () => {
     setPasswordShown(passwordShown ? false : true);
   };
   
 
-  const { register, handleSubmit } = useForm();
-  const onSubmit = data => {
-    
-    console.log(data);
-  };
+  const { register } = useForm();
+ 
+  const handleSignup = async () => {
+    try {
+      const response = await fetch('/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
+      if (response.ok) {
+        // Signup successful, perform necessary actions (e.g., redirect, show success message)
+        console.log('Signup successful');
+      } else {
+        // Signup failed, handle error (e.g., show error message)
+        console.error('Signup failed');
+      }
+    } catch (error) {
+      console.error('An error occurred', error);
+    }
+  };
 
   return (
     <div className='container  mt-sm-4'>
         <center>
         <br/>
         <label>User ID</label>&nbsp;&nbsp;&nbsp;
-        <input type='text' placeholder='Enter Username...'/>
+        <input type='text' placeholder='Enter Username...'value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <br/>
         <br/>
         <label>Password</label>&nbsp;&nbsp;&nbsp;
@@ -40,7 +61,8 @@ const [passwordShown, setPasswordShown] = useState(false);
           }} />
          </View> */}
         <input  placeholder='Enter Password...'type={passwordShown ? "text" : "password"}
-        {...register('Password',{ required: "This is required." })} />
+        {...register('Password',{ required: "This is required." })} value={password}
+          onChange={(e) => setPassword(e.target.value)} />
          <i onClick={togglePasswordVisiblity}>{eye}</i>{" "}
         <br/>
         <br/>
@@ -51,7 +73,7 @@ const [passwordShown, setPasswordShown] = useState(false);
         <label htmlFor='Admin'><span> Admin  </span></label>
         <br/>
         <br/>
-        <button type="submit" onClick={handleSubmit(onSubmit)}>
+        <button type="submit" onClick={handleSignup}>
         Signup
       </button>
         <br/>
